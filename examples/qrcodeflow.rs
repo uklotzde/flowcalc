c133500651212ff6b9741b5646fba553d5b20054
Uwe Klotz <uwe.klotz@gmail.com>
2020-03-25 22:24:53 +0100

    Skip SVG roundtrip in qrcodeflow example

--- examples/qrcodeflow.rs
+++ examples/qrcodeflow.rs
@@ -1,26 +1,13 @@
 use flowcalc::{flow::*, node::*, port::*, Packet};
 
-use std::{cell::RefCell, fmt, rc::Rc, path::{Path, PathBuf}, time::Instant};
+use std::{cell::RefCell, rc::Rc, time::Instant};
 
 use rand::{self, rngs::ThreadRng, Rng as _};
-
-use resvg::Render as SvgRender;
-use usvg::Tree as SvgTree;
-
-#[derive(Clone)]
-pub struct QrCode(qrcodegen::QrCode);
-
-impl fmt::Debug for QrCode {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        f.write_str(&self.0.to_svg_string(0))
-    }
-}
 
 #[derive(Debug, Clone)]
 enum Value {
     Text(String),
-    QrCode(QrCode),
-    FilePath(PathBuf),
+    GrayImage(image::GrayImage),
 }
 
 #[derive(Debug)]
@@ -172,99 +159,11 @@ impl NodeProcessor for TextQrEncoder {
         }
         let input_value = self.input.slot.take();
         if let Some(Value::Text(text)) = input_value {
-            let code = qrcodegen::QrCode::encode_text(&text, qrcodegen::QrCodeEcc::Quartile)
-                .expect("Input string too long");
-            self.output.slot = Some(Value::QrCode(QrCode(code)));
+            let code = qrcode::QrCode::new(text.as_bytes()).expect("QR code");
+            let image = code.render::<image::Luma<_>>().build();
+            self.output.slot = Some(Value::GrayImage(image));
         } else {
             panic!("Missing input string");
-        }
-    }
-
-    fn process_outputs(&mut self, _: AccessToken) {
-        self.input.activate(self.output.is_active());
-    }
-}
-
-struct QrCodeRender {
-    input: Port<Value>,
-    output: Port<Value>,
-    render: Box<dyn SvgRender>,
-}
-
-impl QrCodeRender {
-    fn new() -> Self {
-        Self {
-            input: Port::new(),
-            output: Port::new(),
-            render: resvg::default_backend(),
-        }
-    }
-}
-
-impl Node<Value> for QrCodeRender {
-    fn num_inputs(&self) -> usize {
-        1
-    }
-
-    fn num_outputs(&self) -> usize {
-        1
-    }
-
-    fn dispatch_input_packet(
-        &mut self,
-        _token: AccessToken,
-        _input_index: PortIndex,
-    ) -> Packet<Value> {
-        self.input.dispatch_packet()
-    }
-
-    fn dispatch_output_packet(
-        &mut self,
-        _token: AccessToken,
-        _output_index: PortIndex,
-    ) -> Packet<Value> {
-        self.output.dispatch_packet()
-    }
-
-    fn accept_input_packet(
-        &mut self,
-        _token: AccessToken,
-        _input_index: PortIndex,
-        packet: Packet<Value>,
-    ) {
-        self.input.accept_packet(packet);
-    }
-
-    fn accept_output_packet(
-        &mut self,
-        _token: AccessToken,
-        _output_index: PortIndex,
-        packet: Packet<Value>,
-    ) {
-        self.output.accept_packet(packet);
-    }
-}
-
-impl NodeProcessor for QrCodeRender {
-    fn process_inputs(&mut self, _: AccessToken) {
-        if !self.output.is_active() {
-            return;
-        }
-        let input_value = self.input.slot.take();
-        if let Some(Value::QrCode(code)) = input_value {
-            let svg_tree =
-                SvgTree::from_str(&code.0.to_svg_string(2), &Default::default()).expect("SVG tree");
-            let mut output_image = self.render.render_to_image(&svg_tree, &resvg::Options {
-                background: Some(usvg::Color::white()),
-                fit_to: resvg::FitTo::Width(200),
-                ..Default::default()
-            }).expect("output image");
-            // FIXME: Don't write to disk!
-            let path = Path::new("/tmp/qrcode.png");
-            output_image.save_png(path);
-            self.output.slot = Some(Value::FilePath(path.into()));
-        } else {
-            panic!("Missing QR code input");
         }
     }
 
@@ -339,10 +238,8 @@ impl NodeProcessor for QrTextDecoder {
             return;
         }
         let input_value = self.input.slot.take();
-        if let Some(Value::FilePath(path)) = input_value {
-            let reader = image::io::Reader::open(path).expect("PNG file");
-            let image = reader.decode().expect("PNG image");
-            let results = self.decoder.decode(&image);
+        if let Some(Value::GrayImage(image)) = input_value {
+            let results = self.decoder.decode(&image::DynamicImage::ImageLuma8(image));
             debug_assert!(results.len() <= 1);
             let first_result = results.into_iter().next();
             if let Some(first_result) = first_result {
@@ -366,7 +263,6 @@ fn main() {
     let source = Rc::new(RefCell::new(RandomAsciiTextSource::new(20)));
     let encoder = Rc::new(RefCell::new(TextQrEncoder::new()));
     let decoder = Rc::new(RefCell::new(QrTextDecoder::new()));
-    let render = Rc::new(RefCell::new(QrCodeRender::new()));
     let splitter = Rc::new(RefCell::new(OneToManySplitter::<Value>::new(2)));
     let printer = Rc::new(RefCell::new(DebugPrinterSink::<Value>::new(2)));
 
@@ -374,7 +270,6 @@ fn main() {
     let source_id = flow.add_node(RcProxyNode::new(Rc::clone(&source) as _));
     let encoder_id = flow.add_node(RcProxyNode::new(Rc::clone(&encoder) as _));
     let decoder_id = flow.add_node(RcProxyNode::new(Rc::clone(&decoder) as _));
-    let render_id = flow.add_node(RcProxyNode::new(Rc::clone(&render) as _));
     let splitter_id = flow.add_node(RcProxyNode::new(Rc::clone(&splitter) as _));
     let printer_id = flow.add_node(RcProxyNode::new(Rc::clone(&printer) as _));
     flow.connect(
@@ -410,16 +305,6 @@ fn main() {
     flow.connect(
         Socket {
             node_id: encoder_id,
-            port_index: PortIndex::new(0),
-        },
-        Socket {
-            node_id: render_id,
-            port_index: PortIndex::new(0),
-        },
-    );
-    flow.connect(
-        Socket {
-            node_id: render_id,
             port_index: PortIndex::new(0),
         },
         Socket {
